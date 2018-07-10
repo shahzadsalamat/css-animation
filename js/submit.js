e4fdb8020c587c3fa7c17fe8b4f222c9b1bd1d71
shahzadsalamat <shahzad.salamat@yahoo.com>
2018-07-10 20:02:34 +0300

    added files for sumbit button animation

---
+++ js/submit.js
@@ -0,0 +1,23 @@
+var $button = $('#submit');
+var $reset = $('#reset');
+
+$button.on('click', function(e) {
+  e.preventDefault();
+  setTimeout(function(){
+     $button.addClass('clicked');
+  }, 500);
+  
+  $button.one('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(){
+        // Do something once!
+        $(this).addClass('submited');
+  });
+});
+
+
+
+$reset.on('click', function(e) {
+  e.preventDefault();
+  $button.removeClass('clicked');
+  $button.removeClass('submited');
+  $button.off('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd');
+});
